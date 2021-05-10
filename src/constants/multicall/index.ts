46f57c8f2455223e99e008b59f94bf6ccb0963d5
Fdango <ccguangyao@gmail.com>
2021-05-10 16:06:54 +0800

    first commit

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,9 @@
+import { ChainId } from '@pancakeswap-libs/sdk'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
+  [ChainId.BSCTESTNET]: '0x6d2c2c918d43465fca5FCd66CBC8cD317477A760'
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
